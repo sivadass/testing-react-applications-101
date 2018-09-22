36ad7060880df6a1001cdc3ea8f98609d1406a31
Sivadass N <nsivadass@gmail.com>
2018-09-22 14:56:12 +0530

    Added app tests

--- src/App.js
+++ src/App.js
@@ -2,10 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error: false,
+    };
+
+    this.decrementCounter = this.decrementCounter.bind(this)
+    this.incrementCounter = this.incrementCounter.bind(this)
+  }
+
+  decrementCounter() {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  }
+
+  incrementCounter() {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
   render() {
+    
+    const errorClass = this.state.error ? '' : 'hidden';
+
     return (
-      <div className="App">
-       <h1>Hello Chennai JS</h1>
+      <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+      <div data-test="error-message" className={`error ${errorClass}`}>
+        The counter cannot go below 0
+      </div>
+      <button
+        data-test="increment-button"
+        onClick={this.incrementCounter}
+        >
+        Increment counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={this.decrementCounter}
+        >
+        Decrement counter
+      </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
+
+const setup = (props={}, state=null) => {
+  const wrapper = shallow(<App {...props} />)
+  if (state) wrapper.setState(state);
+  return wrapper;
+}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
 });
